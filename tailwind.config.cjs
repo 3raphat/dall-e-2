30b8f14cdc445984a062916b258c459142bee580
3raphat <treeraphat234@gmail.com>
2023-02-02 22:57:21 +0700

    lasthope

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,10 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
+    "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    require('@catppuccin/tailwindcss')({
+      prefix: 'ctp',
+      defaultFlavour: 'latte'
+    }),
+  ],
 }
